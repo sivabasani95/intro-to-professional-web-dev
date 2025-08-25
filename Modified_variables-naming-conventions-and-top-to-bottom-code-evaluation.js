8391d688f1085b8d68565936df3eaa47e0296068
sivabasani95 <sivabasani95@gmail.com>
2025-08-25 13:34:09 -0500

    Create Modified_variables-naming-conventions-and-top-to-bottom-code-evaluation.js

---
+++ Modified_variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -0,0 +1,11 @@
+let person = "Alice";
+let amountOfItems = 15;
+let totalAmount = 50;
+let carMake = "Nissan";
+let carModel = "Rouge";
+let carColor = "Gray";
+
+let shoppingResult = person + " bought " + amountOfItems + " items for $" + totalAmount + ".";
+let carResult = person + " puts items in their " + carColor + " " + carMake + " " + carModel + ".";
+
+console.log(shoppingResult + " " + carResult);
